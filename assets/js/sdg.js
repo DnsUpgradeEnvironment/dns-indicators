4bd038e193418fad6565ab78c5344c79907d19d8
MoBosse <50657629+MoBosse@users.noreply.github.com>
2022-05-23 09:19:41 +0000

    Deploying to gh-pages from  @ b87f4875cfaf6f76f6ff23998fa492fbf7e45000 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3134,7 +3134,7 @@ var indicatorView = function (model, options) {
 
     view_obj.createSelectionsTable(args);
 
-    view_obj.updateChartTitle(args.chartTitle.replace("<sub>","").replace("</sub>",""));
+    view_obj.updateChartTitle(args.chartTitle.replace("<sub>","").replace("</sub>",""), args.chartSubtitle);
     view_obj.updateSeriesAndUnitElements(args.selectedSeries, args.selectedUnit);
     view_obj.updateUnitElements(args.selectedUnit);
     view_obj.updateTimeSeriesAttributes(args.timeSeriesAttributes);
@@ -3449,7 +3449,7 @@ var indicatorView = function (model, options) {
     return altered;
   }
 
-  this.updateChartTitle = function(chartTitle) {
+  this.updateChartTitle = function(chartTitle, chartSubtitle) {
     if (typeof chartTitle !== 'undefined') {
       $('.chart-title').text(chartTitle);
     }
