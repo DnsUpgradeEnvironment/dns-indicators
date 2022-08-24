141fce3b5c87c9209dc741c9e4e8bbe7221ad813
MoBosse <50657629+MoBosse@users.noreply.github.com>
2022-08-24 15:37:39 +0000

    Deploying to gh-pages from  @ 5e13b91d380a14e09e03eb6212776f8735f82308 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -5973,8 +5973,8 @@ $(function() {
                     var form = L.DomUtil.create('div', 'disaggregation-fieldset-container'),
                         legend = L.DomUtil.create('legend', 'disaggregation-fieldset-legend'),
                         fieldset = L.DomUtil.create('fieldset', 'disaggregation-fieldset'),
-                        field = translations.t(disaggregation.field);
-                    legend.innerHTML = field;
+                        field = disaggregation.field;
+                    legend.innerHTML = translations.t(field);
                     fieldset.append(legend);
                     form.append(fieldset);
                     formInputs.append(form);
