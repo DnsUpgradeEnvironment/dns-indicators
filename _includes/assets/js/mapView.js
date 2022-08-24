4feb92eefb80190977d35f1847dcc2cf41f7acb5
MoBosse <moritz.bosse@destatis.de>
2022-08-24 14:46:16 +0200

    1.3

---
+++ _includes/assets/js/mapView.js
@@ -0,0 +1,21 @@
+var mapView = function () {
+
+  "use strict";
+
+  this.initialise = function(goal, indicatorId, precision, precisionItems, decimalSeparator, dataSchema, viewHelpers, modelHelpers, chartTitles) {
+    $('.map').show();
+    $('#map').sdgMap({
+      indicatorId: indicatorId,
+      goal: goal,
+      mapOptions: {{ site.map_options | jsonify }},
+      mapLayers: {{ site.map_layers | jsonify }},
+      precision: precision,
+      precisionItems: precisionItems,
+      decimalSeparator: decimalSeparator,
+      dataSchema: dataSchema,
+      viewHelpers: viewHelpers,
+      modelHelpers: modelHelpers,
+      chartTitles: chartTitles,
+    });
+  };
+};
