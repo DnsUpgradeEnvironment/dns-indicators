fef980dfa64099d17644a68d7adeebd9c324824c
MoBosse <moritz.bosse@destatis.de>
2022-08-26 15:53:19 +0200

    add legend only if numDisagg >1

--- _includes/assets/js/plugins/leaflet.disaggregationControls.js
+++ _includes/assets/js/plugins/leaflet.disaggregationControls.js
@@ -306,9 +306,10 @@
 
                 var numSeries = this.allSeries.length,
                     numUnits = this.allUnits.length,
+                    numDisaggs = this.allDisaggregations.length,
                     displayForm = this.displayForm;
 
-                if (displayForm && (this.hasDisaggregations || (numSeries > 1 || numUnits > 1))) {
+                if (displayForm && (numDisaggs > 1 || numSeries > 1 || numUnits > 1)) {
 
                     var button = L.DomUtil.create('button', 'disaggregation-button');
                     button.innerHTML = translations.indicator.change_breakdowns;
