fb134ba353d213755f3086e4c9828cf6717c614b
MoBosse <moritz.bosse@destatis.de>
2022-09-15 08:28:26 +0200

    backgroundColor annotation label per graph type

--- _includes/components/charts/annotation_presets.js
+++ _includes/components/charts/annotation_presets.js
@@ -7,7 +7,16 @@ opensdg.annotationPresets = {
         borderDash: [10, 5],
         borderWidth: 1,
         label: {
-            backgroundColor: 'rgba(255,255,255,0.6)',
+            backgroundColor: function() {
+                var re = '''
+                if (this.type == 'line') {
+                  re.push("'rgba(255,255,255,0.6)',")
+                }
+                else {
+                  re.push("'rgba(255,255,255,0.2)',")
+                }
+                return re
+              },
             color: 'black',
         },
         // This "highContrast" overrides colors when in high-contrast mode.
