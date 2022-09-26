cd9142cf039db1aa7afb1d7f9e1317beed8c1d4b
MoBosse <moritz.bosse@destatis.de>
2022-09-26 12:45:23 +0200

    set table width allways to 100%

--- _includes/assets/js/view/tableHelpers.js
+++ _includes/assets/js/view/tableHelpers.js
@@ -220,28 +220,28 @@ function setDataTableWidth(table) {
     });
 
     table.removeAttr('style width');
-
-    var totalWidth = 0;
-    var column = 0;
-    table.find('thead th').each(function () {
-        column += 1;
-        if ($(this).data('width')) {
-            totalWidth += $(this).data('width');
-            console.log('a) Column ', column, ': ',  $(this).data('width'), ', Total: ' + totalWidth);
-        } else {
-            totalWidth += $(this).width();
-            console.log('b) Column ', column + ': ',  $(this).width(), ', Total: ' + totalWidth);
-        }
-    });
+    table.css('width', '100%');
+    // var totalWidth = 0;
+    // var column = 0;
+    // table.find('thead th').each(function () {
+    //     column += 1;
+    //     if ($(this).data('width')) {
+    //         totalWidth += $(this).data('width');
+    //         console.log('a) Column ', column, ': ',  $(this).data('width'), ', Total: ' + totalWidth);
+    //     } else {
+    //         totalWidth += $(this).width();
+    //         console.log('b) Column ', column + ': ',  $(this).width(), ', Total: ' + totalWidth);
+    //     }
+    // });
 
     // ascertain whether the table should be width 100% or explicit width:
-    var containerWidth = table.closest('.dataTables_wrapper').width();
-    console.log('Table: ', totalWidth, 'Container: ', containerWidth);
-    if (totalWidth > containerWidth) {
-        table.css('width', totalWidth + 'px');
-    } else {
-        table.css('width', '100%');
-    }
+    // var containerWidth = table.closest('.dataTables_wrapper').width();
+    // console.log('Table: ', totalWidth, 'Container: ', containerWidth);
+    // if (totalWidth > containerWidth) {
+    //     table.css('width', totalWidth + 'px');
+    // } else {
+    //     table.css('width', '100%');
+    // }
 }
 
 /**
