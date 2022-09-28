99df3d982a01aaade49ffb1153803a65995c0046
MoBosse <50657629+MoBosse@users.noreply.github.com>
2022-09-28 14:52:37 +0000

    Deploying to gh-pages from  @ a382fb2582ef9901c093381e01da97e88901da69 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3435,7 +3435,8 @@ function updateHeadlineColor(contrast, chartInfo, indicatorId) {
     //return isHighContrast(contrast) ? '#FFDD00' : '#e5243b#dda63a#4c9f38#c5192d#ff3a21#26bde2#fcc30b#a21942#fd6925#dd1367#fd9d24#bf8b2e#3f7e44#0a97d9#56c02b#00689d#19486a';
 function getHeadlineColor(contrast, goalNumber) {
   console.log("goalNumber: ", goalNumber);
-    return isHighContrast(contrast) ? '#FFDD00' : '#00006a';
+  var htmlString = '#00006a';
+    return isHighContrast(contrast) ? '#FFDD00' : htmlString;
 }
 
 /**
