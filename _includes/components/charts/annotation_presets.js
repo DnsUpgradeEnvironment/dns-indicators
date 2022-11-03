f3eed4ac1d217ff0a4999cb183904e10c61ae6c3
MoBosse <moritz.bosse@destatis.de>
2022-11-03 09:11:58 +0100

    del x adjust

--- _includes/components/charts/annotation_presets.js
+++ _includes/components/charts/annotation_presets.js
@@ -79,7 +79,7 @@ opensdg.annotationPresets = {
         backgroundColor: 'grey',
         borderWidth: 0,
         radius: 8,
-        xAdjust: -0.4,
+        //xAdjust: -0.4,
         xScaleID: 'x',
         yScaleID: 'y',
         xMin: 2,
